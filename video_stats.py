f6eb2fe4927b24af52a67402c8672169d396dcef
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2026-02-26 06:19:44 +0700

    feat: get video statistics

--- video_stats.py
+++ video_stats.py
@@ -57,10 +57,69 @@ def get_video_ids(playlist_id: str, api_key: str = API_KEY, max_results: int = M
             if not page_token:
                 break
             pake_token = page_token
+            
         return video_ids
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
+def extract_video_stats(video_ids: list, api_key: str = API_KEY) -> list:
+    """Extract video statistics for a list of video ids.
+
+    Args:
+        video_ids (list): List of video ids to extract statistics for.
+        api_key (str, optional): API key for YouTube Data API. Defaults to API_KEY.
+
+    Returns:
+        list: A list of dictionaries containing video statistics.
+    """
+    stats = []
+    BASE_URL = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id=FC53Ht_J1Tk&key={api_key}'
+    
+    def batch_video_ids(video_ids: list, batch_size: int) -> list:
+        """Batch video ids into smaller lists.
+
+        Args:
+            video_ids (list): List of video ids to be batched.
+            batch_size (int, optional): The size of each batch. Defaults to 50.
+
+        Returns:
+            list: A list of batches, where each batch is a list of video ids.
+        """
+        return [video_ids[i:i + batch_size] for i in range(0, len(video_ids), batch_size)]
+        
+    try:
+        for batch in batch_video_ids(video_ids, MAX_RESULTS):
+            video_ids_str = ','.join(batch)
+            url = BASE_URL + f'&id={video_ids_str}'
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            
+            for item in data.get('items',[]):
+                video_id = item['id']
+                snippet = item['snippet']
+                content_details = item['contentDetails']
+                statistic = item['statistics']
+                
+                video_data = {
+                    'video_id' : video_id,
+                    'title' : snippet['title'],
+                    'published_at' : snippet['publishedAt'],
+                    'duration' : content_details['duration'],
+                    'view_count' : statistic.get('viewCount', 0),
+                    'like_count' : statistic.get('likeCount', 0),
+                    'comment_count' : statistic.get('commentCount', 0)
+                }
+                
+                stats.append(video_data)
+            
+        return stats
+    
     except requests.exceptions.RequestException as e:
         raise e
 
 if __name__ == "__main__":
     test = get_playlist_id()
-    print(get_video_ids(test))
+    video_ids = get_video_ids(test)
+    extract_video_stats(video_ids)
