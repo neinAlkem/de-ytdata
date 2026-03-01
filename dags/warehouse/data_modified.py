0d744e693dbce8e735a8f22f243b2df3c30190fe
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2026-03-01 22:29:40 +0700

    fix: update sql bug potential

--- dags/warehouse/data_modified.py
+++ dags/warehouse/data_modified.py
@@ -29,15 +29,16 @@ def update_row(cur, conn, schema_name, row) -> None:
             update_query = f"""
                 UPDATE {schema_name}.{table}
                 SET title = %s, view_count = %s, like_count = %s, comment_count = %s
-                WHERE video_id = %s AND published_at = %s AND duration = %s
-            """
+                WHERE video_id = %s AND duration = %s
+            """ 
         else:
             update_query = f"""
                 UPDATE {schema_name}.{table}
                 SET title = %s, view_count = %s, like_count = %s, comment_count = %s
-                WHERE video_id = %s AND published_at = %s AND duration::time = %s::time
+                WHERE video_id = %s AND duration = %s::time
             """
-        cur.execute(update_query,  row[1:] + (row[0],))
+        params = (row[1], row[4], row[5], row[6], row[0], row[3])
+        cur.execute(update_query, params)
         conn.commit()
         logger.info(f"Updating row with video_id '{row[0]}' in schema '{schema_name}' successful.")
     except Exception as e:
@@ -47,11 +48,11 @@ def update_row(cur, conn, schema_name, row) -> None:
     
 def delete_row(cur, conn, schema_name, video_id) -> None:
     try:
-        video_id_delete = f"""({', '.join([f"'{id}'" for id in video_id])})"""
-        cur.execute(f"DELETE FROM {schema_name}.{table} WHERE video_id IN {video_id_delete};")
+        placeholders = ','.join(['%s'] * len(video_id))
+        cur.execute(f"DELETE FROM {schema_name}.{table} WHERE video_id IN ({placeholders});", tuple(video_id))
         conn.commit()
-        logger.info(f"Deleting rows with video_id in '{video_id_delete}' from schema '{schema_name}' successful.")
+        logger.info(f"Deleting rows with video_id in '{video_id}' from schema '{schema_name}' successful.")
     except Exception as e:
-        logger.error(f"Error deleting rows from schema with video ids in '{video_id_delete}': {e}")
+        logger.error(f"Error deleting rows from schema with video ids in '{video_id}': {e}")
         conn.rollback()
         raise e
