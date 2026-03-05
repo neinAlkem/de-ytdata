e28d7499df2ccac808a564a04fa44461a28764ac
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2026-03-06 06:28:03 +0700

    feat: add trigger to dags deps

--- dags/main.py
+++ dags/main.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from api.extract_video_stats import get_playlist_id, get_video_ids, extract_video_stats, save_stats_to_json
 from warehouse.dwh import stg_table, dbo_table
 from data_quality_check.soda import check_data_quality
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 local_timezone = pendulum.timezone("Asia/Jakarta")
 
@@ -26,14 +27,21 @@ with DAG(
     description='Script to extract video statistics from YouTube Data API and save it as JSON file',
     schedule_interval='0 14 * * *',
     catchup=False,
-) as dag:
+) as dag_produce:
     
     task_get_playlist = get_playlist_id()
     task_get_video_ids = get_video_ids(task_get_playlist)
     task_get_stats = extract_video_stats(task_get_video_ids)
     task_save_json = save_stats_to_json(task_get_stats, 'video_stats')
     
-    task_get_playlist >> task_get_video_ids >> task_get_stats >> task_save_json
+    trigger_update_db = TriggerDagRunOperator(
+        task_id='trigger_update_db',
+        trigger_dag_id='update_db',
+        wait_for_completion=True,
+        reset_dag_run=True
+    )
+    
+    task_get_playlist >> task_get_video_ids >> task_get_stats >> task_save_json >> trigger_update_db
     
 with DAG(
     'update_db',
@@ -41,12 +49,19 @@ with DAG(
     description='Script to update database with video statistics from YouTube Data API',
     schedule_interval='0 15 * * *',
     catchup=False,
-) as dag:
+) as dag_update_db:
     
     task_stg_table = stg_table()
     task_dbo_table = dbo_table()
     
-    task_stg_table >> task_dbo_table
+    trigger_data_qc = TriggerDagRunOperator(
+        task_id='trigger_data_quality_check',
+        trigger_dag_id='data_quality_check',
+        wait_for_completion=True,
+        reset_dag_run=True
+    )
+    
+    task_stg_table >> task_dbo_table >> trigger_data_qc
 
 with DAG(
     'data_quality_check',
@@ -54,7 +69,7 @@ with DAG(
     description='Data quality check using Soda for both layer',
     schedule_interval='0 16 * * *',
     catchup=False,
-) as dag:
+) as dag_quality:
     
     task_stg_table = check_data_quality('stg')
     task_dbo_table = check_data_quality('dbo')
